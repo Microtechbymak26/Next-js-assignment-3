2a16fb94a27e56efb1ca345ed7bd71ebb1c66a5b
Microtechbymak26 <usubka786@Gmail.com>
2024-10-23 06:01:46 -0700

    ...

---
+++ app/components/About.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <section className='container  px-6 p-10 bg-[#B9E5E8]'>
+        <h2 className='text-4xl font-bold text-center text-black-800 mb-8'>
+            About Us
+        </h2>
+        <p className='text-black-600 mb-8'>
+            This is a simple web application built using Next.js and Tailwind CSS. It demonstrates the capabilities of Next.js for server-side rendering and static site generation, as well as the use of Tailwind CSS for styling.
+        </p>
+        <p className='text-black-600 mb-8'>
+        The application is designed to be responsive and mobile-friendly, ensuring that users can access and manage the products on their devices of all sizes.
+        </p>
+        <p className='text-black-600 mb-8'>
+        The source code is available on GitHub at <a href="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" className='text-black-500 hover:underline'>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</a>
+        </p>
+        <p className='text-black-600 mb-8'>
+        Thanks for visiting
+        </p>
+        <p className='text-black-600 mb-8'>
+        Ateeb Kashif, 2024
+        </p>
+    </section>
+    </>
+  )
+}
+
+export default About
---
+++ app/components/portfolio.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from "next/image";
+import logo from "./Image1.jpg"
+import Logo from "./Image2.jpg"
+import logo1 from "./Image3.jpg"
+
+const portfolio = () => {
+  return (
+   <>
+   <section className='container mx-auto px-6 p-10 bg-[#7AB2D3]'>
+    <h2 className='text-4xl font-bold text-center text-gray-800 mb-8'>Services</h2>
+    <p className='text-center text-black-600 mb-8'>Here are some of the services we provide</p>
+    <div className='flex flex-wrap'>
+        <div className='w-full md:w-1/3 px-2 mb-4'>
+            <Image src={logo} alt="image 1" className='w-full h-auto rounded-lg shadow-lg'/>
+            <h1></h1>
+        </div>
+        <div className='w-full md:w-1/3 px-2 mb-4'>
+          <Image src={logo1} alt='image 2'/>
+        </div>
+        <div className='w-full md:w-1/3 px-2 mb-4'>
+        <Image src={Logo} alt="image 1" className='w-full h-auto rounded-lg shadow-lg'/>
+        </div>
+        
+        
+
+    </div>
+   </section>
+   </>
+  )
+}
+
+export default portfolio
